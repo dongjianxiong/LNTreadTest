bfe8c805b9cc85c4b043bf42e145a81ece04f56b
scad-build <scad-build@sohu-inc.com>
2018-11-06 11:50:14 +0800

    Initial Commit

---
+++ LNTreadTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNTreadTest
+//
+//  Created by ioser on 2018/11/6.
+//  Copyright © 2018年 Lenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LNTreadTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LNTreadTest
+//
+//  Created by ioser on 2018/11/6.
+//  Copyright © 2018年 Lenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
